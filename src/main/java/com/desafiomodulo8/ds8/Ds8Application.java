aec189c3345e3a98464ecf6ad42c15414576f0c3
maurodamatta <maurodamatta00@gmail.com>
2023-02-10 13:49:34 -0300

    Desafio qse finalizado com erros no INSERT

---
+++ src/main/java/com/desafiomodulo8/ds8/Ds8Application.java
@@ -0,0 +1,13 @@
+package com.desafiomodulo8.ds8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ds8Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/desafiomodulo8/ds8/entities/Bloco.java
@@ -0,0 +1,60 @@
+package com.desafiomodulo8.ds8.entities;
+
+import java.time.Instant;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant inicio;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant fim;
+
+    @ManyToOne
+    @JoinColumn(name = "atividade_id")
+    private Atividade atividades;
+
+    public Bloco(){
+    }
+
+    public Bloco(Integer id, Instant inicio, Instant fim) {
+        this.id = id;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Instant inicio) {
+        this.inicio = inicio;
+    }
+
+    public Instant getFim() {
+        return fim;
+    }
+
+    public void setFim(Instant fim) {
+        this.fim = fim;
+    }
+
+    public Atividade getAtividades() {
+        return atividades;
+    }
+
+    
+}
